19aad0beada5d29840df48a78d8898b2ab107ee0
Kamil <kamil.bulat@interia.pl>
2016-10-28 23:35:59 +0200

    alek

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
     //alert("jquery dziala \nculture date")\
+    
     $("#bt1").on("click",function(){
         $("#start").fadeOut(500, function(){
+            $("#main").fadeOut(0)
             $("#main").fadeIn(500)
-        });
+        })
     })
 })
